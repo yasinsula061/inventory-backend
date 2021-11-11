faa687905b09163a773643ec236d61ff0a266b74
Yasin SULA <yasinsulak61@gmail.com>
2021-11-11 23:49:04 +0300

    Edit example.

--- src/main/java/com/sula/inventory/Application.java
+++ src/main/java/com/sula/inventory/Application.java
@@ -20,7 +20,7 @@ public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) throws Exception {
 
         logger.info("Spring Versiyon : " + SpringVersion.getVersion());
-        logger.info("SULA-WEB");
+        logger.info("inventory Starting .. Versiyon");
         SpringApplication.run(Application.class, args);
     }
 }
